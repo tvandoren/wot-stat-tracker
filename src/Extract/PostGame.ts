fe99386b0debdbc210d9fe013b8ce2d6ee4d609b
Trevor VanDoren <vandoren.trevor@gmail.com>
2024-03-02 22:10:43 -0800

    temporarily fix ts shit

--- src/Extract/PostGame.ts
+++ src/Extract/PostGame.ts
@@ -1,5 +1,9 @@
 import type { IPostGameInfo, IPostGameMetadata } from '../types';
+import { getLogger } from '../utils/Logger';
+
+const logger = getLogger('PostGame');
 
 export function getPostGameData(postGame: IPostGameMetadata, filePath: string): IPostGameInfo | null {
+  logger.debug({ filePath, hasPostGame: Boolean(postGame) }, 'logging to make typscript stfu');
   return {};
 }
